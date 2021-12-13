e95700d3f5fdedd789be815a6237e636d297f645
Evgeniy <evgeniy.kulinich23@gmail.com>
2021-12-13 10:07:47 +0300

    fixed value legend

--- src/components/legend/index.js
+++ src/components/legend/index.js
@@ -8,8 +8,8 @@ export const Legend = () => {
       <div style={{ "--color": "#FEB24C" }}>50000 - 100000</div>
       <div style={{ "--color": "#FD8D3C" }}>100000 - 500000</div>
       <div style={{ "--color": "#FC4E2A" }}>500000 - 1000000</div>
-      <div style={{ "--color": "#E31A1C" }}>1000,000 - 5000000</div>
-      <div style={{ "--color": "#BD0026" }}>5000,000 - 30000000</div>
+      <div style={{ "--color": "#E31A1C" }}>1000000 - 5000000</div>
+      <div style={{ "--color": "#BD0026" }}>5000000 - 30000000</div>
       <div style={{ "--color": "#800026" }}>more 30000000</div>
     </WrapperLegend>
   );
